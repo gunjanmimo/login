bce8a4459c31ff1d2f21fbc650c047a5897eebc6
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-08-02 01:59:11 +0530

    new

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
+const bcrypt = require("bcrypt")
 const port = process.env.PORT || 3000
 
+app.use(express.urlencoded({ extended: false }))
 
-
+const user = []
 
 // path declearation 
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -26,6 +28,25 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index')
 })
+app.get('/login', (req, res) => {
+    res.render("login")
+})
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post("", (req, res) => {
+
+})
+
+
+
+
+app.post("", (req, res) => {
+    req.body.name
+})
+
+
 
 
 
