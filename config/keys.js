8ee7b56e1535aedf7907e97f638e94b38b49fdbc
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-08-06 19:04:00 +0530

    new

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: "82087811148-3s1i2vle1lftf1rv7q1dq9t2jbchpkq6.apps.googleusercontent.com",
+    googleClientSecret: "uLIXFE11U5i2xq2UAnLhYV0F"
+}
--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
 const app = express()
-const bcrypt = require("bcrypt")
+const GoogleStrategy = require("passport-google-oauth20")
+//const bcrypt = require("bcrypt")
+const keys = require("../config/keys")
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: false }))
@@ -18,26 +20,42 @@ const partialsPath = path.join(__dirname, '../templates/partials')
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
-    // Setup static directory to serve
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+// google oauth
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log("acess token: ", accessToken)
+        console.log("refresh token:", refreshToken)
+        console.log("profile: ", profile)
+        console.log("done: ", cb)
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
+
+app.get("/auth/google", passport.authenticate("google", {
+    scope: ['profile', 'email']
+}))
+
+
+app.get("/auth/google/callback", passport.authenticate("google", {
+    scope: ['profile', 'email']
+}))
 
 
 
 app.get('', (req, res) => {
     res.render('index')
 })
-app.get('/login', (req, res) => {
-    res.render("login")
-})
-app.get('/register', (req, res) => {
-    res.render('register')
-})
 
-app.post("", (req, res) => {
-
-})
 
 
 
