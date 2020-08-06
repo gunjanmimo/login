2d3b4f3b7389d205e1cf64c743d2cf56c0bc15b8
gunjanmimo <gunjan.mtbpaul@gmail.com>
2020-08-06 20:03:36 +0530

    new

--- src/app.js
+++ src/app.js
@@ -66,6 +66,41 @@ app.get("/auth/google/callback", passport.authenticate("google", {
 
 
 // ! facebook oauth
+passport.use(new FacebookStrategy({
+    clientID: "1252574065074048",
+    clientSecret: "144a8861bbdfe41c9b12eeb40f6ba717",
+    callbackURL: "/auth/facebook/callback"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+    });
+
+
+
+// passport.serializeUser(function (user, done) {
+//     done(null, user);
+// });
+
+// passport.deserializeUser(function (obj, done) {
+//     done(null, obj);
+// });
+
+
+// // Use the FacebookStrategy within Passport.
+
 // passport.use(new FacebookStrategy({
 //     clientID: "1252574065074048",
 //     clientSecret: "144a8861bbdfe41c9b12eeb40f6ba717",
@@ -75,72 +110,45 @@ app.get("/auth/google/callback", passport.authenticate("google", {
 //         process.nextTick(function () {
 //             //Check whether the User exists or not using profile.id
 //             if (config.use_database) {
-//                 //Further code of Database.
+//                 // if sets to true
+//                 pool.query("SELECT * from user_info where user_id=" + profile.id, (err, rows) => {
+//                     if (err) throw err;
+//                     if (rows && rows.length === 0) {
+//                         console.log("There is no such user, adding now");
+//                         pool.query("INSERT into user_info(user_id,user_name) VALUES('" + profile.id + "','" + profile.username + "')");
+//                     } else {
+//                         console.log("User already exists in database");
+//                     }
+//                 });
 //             }
 //             return done(null, profile);
 //         });
 //     }
-
 // ));
-passport.serializeUser(function (user, done) {
-    done(null, user);
-});
 
-passport.deserializeUser(function (obj, done) {
-    done(null, obj);
-});
+// app.get('/account', ensureAuthenticated, function (req, res) {
+//     res.render('account', { user: req.user });
+// });
 
-
-// Use the FacebookStrategy within Passport.
-
-passport.use(new FacebookStrategy({
-    clientID: "1252574065074048",
-    clientSecret: "144a8861bbdfe41c9b12eeb40f6ba717",
-    callbackURL: "/auth/facebook/callback"
-},
-    function (accessToken, refreshToken, profile, done) {
-        process.nextTick(function () {
-            //Check whether the User exists or not using profile.id
-            if (config.use_database) {
-                // if sets to true
-                pool.query("SELECT * from user_info where user_id=" + profile.id, (err, rows) => {
-                    if (err) throw err;
-                    if (rows && rows.length === 0) {
-                        console.log("There is no such user, adding now");
-                        pool.query("INSERT into user_info(user_id,user_name) VALUES('" + profile.id + "','" + profile.username + "')");
-                    } else {
-                        console.log("User already exists in database");
-                    }
-                });
-            }
-            return done(null, profile);
-        });
-    }
-));
-
-app.get('/account', ensureAuthenticated, function (req, res) {
-    res.render('account', { user: req.user });
-});
-
-app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
+// app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 
-app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
-    function (req, res) {
-        res.redirect('/');
-    });
+// app.get('/auth/facebook/callback',
+//     passport.authenticate('facebook', { successRedirect: '/', failureRedirect: '/login' }),
+//     function (req, res) {
+//         res.redirect('/');
+//     });
 
-app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
+// app.get('/logout', function (req, res) {
+//     req.logout();
+//     res.redirect('/');
+// });
 
 
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login')
-}
+// function ensureAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) { return next(); }
+//     res.redirect('/login')
+// }
 
 
 
